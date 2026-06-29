3673d3a13cb7071018135a97be0663edb7e80313
Ubuntu <ubuntu@ip-172-31-33-230.eu-central-1.compute.internal>
2026-06-29 15:19:36 +0000

    Add Packages page with Create button

---
+++ frontend/src/app/settings/packages/page.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import React from 'react';
+import { Box, Button } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import PageContents from '@/components/PageContents';
+import { useTranslation } from 'react-i18next';
+
+export default function PackagesPage() {
+    const { t } = useTranslation();
+
+    return (
+        <PageContents>
+            <Box sx={{ p: 3 }}>
+                <Button
+                    variant='contained'
+                    startIcon={<AddIcon />}
+                    sx={{
+                        borderRadius: '8px',
+                        bgcolor: '#00abbe',
+                        '&:hover': { bgcolor: '#009aaa' },
+                        textTransform: 'none',
+                        fontWeight: 600,
+                    }}
+                >
+                    {t('Create')}
+                </Button>
+            </Box>
+        </PageContents>
+    );
+}
